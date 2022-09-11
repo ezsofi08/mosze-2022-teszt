6516d522fdfdf0736fd328b14fb4e7f0ae919226
ezsofi08 <99867208+ezsofi08@users.noreply.github.com>
2022-09-12 00:21:32 +0200

    main3
    Javított main

--- main.cpp
+++ main.cpp
@@ -5,26 +5,28 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     //NELEMENTS helyett N_ELEMENTS
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
     
     //aposztof helyett idézőjel, sor végén hiányzó pontosvessző
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa \n";
         
     //hiányos for ciklus
-    for (int i = 0;)
+    for (int i = 0;i<100;i++)
     {
         //i*2 helyett (i+1)*2
-        b[i] = i * 2;
+        b[i] = (i+1) * 2;
     }
     //i helyett i<100
-    for (int i = 0; i; i++)
+    for (int i = 0; i<100; i++)
     {
         // az eredmény nincs kiírva
         std::cout << "Ertek:"
+        std::cout << b[i];
+        std::cout << "\n"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     //atlag nincs deklarálva
-    int atlag;
+    int atlag=0;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
         atlag += b[i]
