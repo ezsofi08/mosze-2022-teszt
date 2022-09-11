cff7b46cdfe8aa17a1b8244fc991f9956c4956cd
ezsofi08 <99867208+ezsofi08@users.noreply.github.com>
2022-09-12 00:16:43 +0200

    main2
    hibákkal kommentált új main

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
+    //NELEMENTS helyett N_ELEMENTS
     int *b = new int[NELEMENTS];
+    
+    //aposztof helyett idézőjel, sor végén hiányzó pontosvessző
     std::cout << '1-100 ertekek duplazasa'
+        
+    //hiányos for ciklus
     for (int i = 0;)
     {
+        //i*2 helyett (i+1)*2
         b[i] = i * 2;
     }
+    //i helyett i<100
     for (int i = 0; i; i++)
     {
+        // az eredmény nincs kiírva
         std::cout << "Ertek:"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
+    //atlag nincs deklarálva
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
